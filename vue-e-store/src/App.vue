3b120d1843c10a9d6dbf340f74a4b7ae83679ec4
dvdice <valitovdamir73@gmail.com>
2024-10-31 23:19:10 +0400

    Добавил инпут поиска и селектор фильтра, в drawer добавил кнопку "Оформить заказ" и поля суммы заказа и доставки

--- vue-e-store/src/App.vue
+++ vue-e-store/src/App.vue
@@ -4,18 +4,38 @@
     import Drawer from '@/components/Drawer.vue'
 </script>
 <template>
-    <drawer/>
+<!--    <drawer/>-->
     <div class="bg-white w-auto m-auto h-screen  rounded-xl shadow-xl mt-5">
         <Header/>
-        <div class="p-10 flex flex-row flex-wrap gap-5">
-            <product-card price="150" product-name="ХУЙ"></product-card>
-            <product-card price="150" product-name="ХУЙ"></product-card>
-            <product-card price="150" product-name="ХУЙ"></product-card>
-            <product-card price="150" product-name="ХУЙ"></product-card>
-            <product-card price="150" product-name="ХУЙ"></product-card>
-            <product-card price="150" product-name="ХУЙ"></product-card>
-            <product-card price="150" product-name="ХУЙ"></product-card>
-            <product-card price="150" product-name="ХУЙ"></product-card>
+
+        <div class="p-5">
+            <div class="flex justify-between items-center">
+                <h1 class="text-3xl font-bold ml-5">Товары дня!</h1>
+
+                <div class="flex items-center gap-4"><select class="py-2 px-3 border rounded-md">
+                    <option>По названию</option>
+                    <option>По цене (дешевле)</option>
+                    <option>По цене (дороже)</option>
+                </select>
+
+                    <div class="relative pr-5">
+                        <img class="absolute left-3 top-2" src="/search.svg" alt="">
+                        <input class="border border-gray-300 rounded-md py-1 pl-10 pr-4" placeholder="Поиск..." />
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="p-10 flex flex-row flex-wrap gap-5">
+                <product-card price="150" product-name="ХУЙ"></product-card>
+                <product-card price="150" product-name="ХУЙ"></product-card>
+                <product-card price="150" product-name="ХУЙ"></product-card>
+                <product-card price="150" product-name="ХУЙ"></product-card>
+                <product-card price="150" product-name="ХУЙ"></product-card>
+                <product-card price="150" product-name="ХУЙ"></product-card>
+                <product-card price="150" product-name="ХУЙ"></product-card>
+                <product-card price="150" product-name="ХУЙ"></product-card>
+            </div>
         </div>
     </div>
 </template>
--- vue-e-store/src/components/Drawer.vue
+++ vue-e-store/src/components/Drawer.vue
@@ -6,7 +6,7 @@ import CartItem from '@/components/CartItem.vue'
 <template>
     <div class="fixed top-0 left-0 h-full w-full bg-black z-10 opacity-70"></div>
 
-    <div class="bg-white h-full fixed right-0 top-0 z-20 p-8 w-96">
+    <div class="bg-white h-full fixed right-0 top-0 z-20 p-8 w-3/12">
         <div class="flex items-center gap-5">
             <svg class="cursor-pointer rotate-180 hover:opacity-70 transition" width="16" height="14" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M1 7H14.7143" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
@@ -20,12 +20,18 @@ import CartItem from '@/components/CartItem.vue'
         <cart-item></cart-item>
         <cart-item></cart-item>
 
-        <div>
+        <div class="mt-2">
             <div class="flex">
                 <span>Итого:</span>
                 <div class="flex-1"></div>
                 <b>12990 ₽</b>
             </div>
+            <div class="flex">
+                <span>Доставка:</span>
+                <div class="flex-1"></div>
+                <b>350 ₽</b>
+            </div>
+            <button class="rounded-xl w-full mt-2 p-3 bg-lime-400 hover:bg-lime-200">Оформить заказ</button>
         </div>
     </div>
 </template>
